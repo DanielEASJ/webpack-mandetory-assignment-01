0eff4452581e8206facef7d9691b88a1d147d63a
DanielEASJ <31865128+DanielEASJ@users.noreply.github.com>
2018-10-10 15:59:09 +0200

    changed from button to RadioButton setup

--- src/js/index.ts
+++ src/js/index.ts
@@ -2,17 +2,28 @@ let factor: number = 28.34952;
 let input: HTMLInputElement = <HTMLInputElement> document.getElementById("InputField");
 let outputDiv: HTMLDivElement = <HTMLDivElement> document.getElementById("ResultOutput");
 
-let ButtonToGrams: HTMLButtonElement = <HTMLButtonElement> document.getElementById("ButtonToGrams");
-let ButtonToOunces: HTMLButtonElement = <HTMLButtonElement> document.getElementById("ButtonToOunces");
+// let ButtonToGrams: HTMLButtonElement = <HTMLButtonElement> document.getElementById("ButtonToGrams");
+// let ButtonToOunces: HTMLButtonElement = <HTMLButtonElement> document.getElementById("ButtonToOunces");
 
-ButtonToGrams.addEventListener("click", toGrams)
-ButtonToOunces.addEventListener("click",toOunces)
+let RadioButtonToGrams: HTMLInputElement = <HTMLInputElement> document.getElementById("RadioToGram");
+let RadioButtonToOunces: HTMLInputElement = <HTMLInputElement> document.getElementById("RadioToOunce");
+
+// ButtonToGrams.addEventListener("click", toGrams)
+// ButtonToOunces.addEventListener("click",toOunces)
+
+RadioButtonToGrams.addEventListener("checked", toGrams)
+RadioButtonToOunces.addEventListener("checked", toOunces)
+
+
+// input.addEventListener("change", methodSelected);
+input.addEventListener("input", methodSelected);
 
 function toGrams () 
 { 
     outputDiv.innerHTML = (Number(input.value) * factor).toString() + " g.";
     // console.log (outputDiv);
     // console.log ((Number(input.value) * factor).toString());
+
 }
 
 function toOunces () 
@@ -20,6 +31,27 @@ function toOunces ()
     outputDiv.innerHTML = (Number(input.value) / factor).toString() + " oz.";
     // console.log (outputDiv);
     // console.log ((Number(input.value) * factor).toString());
+}
+
+// determins whitch funktion to run based on radio button selected
+function methodSelected()
+{
+    console.log(input.value)
+
+    if(RadioButtonToGrams.checked == true)
+    {
+        toGrams()
+    }
+
+    else if(RadioButtonToOunces.checked == true)
+    {
+        toOunces()
+    }
+
+    else
+    {
+        outputDiv.innerHTML = "selected a conversion radio button first"
+    }
 }
 
 
